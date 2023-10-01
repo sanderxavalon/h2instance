2b36ae64edbb7591c99e25c7c1b19d487407907b
DESKTOP-U2BJ55P\Sander <sanderxavalon@gmail.com>
2023-10-01 11:18:14 +0800

    Init

---
+++ src/main/java/com/tibame/h2instance/H2instanceApplication.java
@@ -0,0 +1,26 @@
+package com.tibame.h2instance;
+
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.annotation.PostConstruct;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class H2instanceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(H2instanceApplication.class, args);
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseServer() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
+	}
+}
---
+++ src/test/java/com/tibame/h2instance/H2instanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibame.h2instance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2instanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
